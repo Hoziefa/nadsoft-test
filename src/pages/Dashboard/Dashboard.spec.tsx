a8a85cbf29fa2b14ac3440ff46605d0a2b989d3b
Hoziefa Alhassan <hoziefaalhasan@gmail.com>
2023-03-09 23:01:27 +0300

    Build unit-test for the Dashboard

--- src/pages/Dashboard/Dashboard.spec.tsx
+++ src/pages/Dashboard/Dashboard.spec.tsx
@@ -1,12 +1,13 @@
-import {render, screen} from "@testing-library/react";
+import {fireEvent, render, screen} from "@testing-library/react";
 import {QueryClient, QueryClientProvider} from "react-query";
 import {rest} from "msw";
 import {setupServer} from "msw/node";
 
 import Dashboard from "./Dashboard";
 import {IApiForm} from "../../api/countries";
+import {MemoryRouter} from "react-router-dom";
 
-const logsHandler = rest.get("https://api.covid19api.com/summary", (req, res, ctx) => {
+const statisticsHandler = rest.get("https://api.covid19api.com/summary", (req, res, ctx) => {
   return res(
     ctx.status(202),
     ctx.json<IApiForm>({
@@ -15,22 +16,37 @@ const logsHandler = rest.get("https://api.covid19api.com/summary", (req, res, ct
         TotalDeaths: 2,
         TotalRecovered: 8,
       },
-      Countries: [],
+      Countries: [
+        {
+          Country: "Jordan",
+          TotalConfirmed: 100,
+          TotalDeaths: 20,
+          TotalRecovered: 80,
+        },
+        {
+          Country: "Saudi Arabia",
+          TotalConfirmed: 200,
+          TotalDeaths: 50,
+          TotalRecovered: 50,
+        },
+      ],
     }),
   );
 });
 
-const server = setupServer(logsHandler);
+const server = setupServer(statisticsHandler);
 
 const renderWithQuery = (queryClient: QueryClient) => {
   return render(
     <QueryClientProvider client={queryClient}>
-      <Dashboard />
+      <MemoryRouter initialEntries={["?name="]}>
+        <Dashboard />
+      </MemoryRouter>
     </QueryClientProvider>,
   );
 };
 
-describe("<Logger /> Test", (): void => {
+describe("<Dashboard /> Test", (): void => {
   const queryClient = new QueryClient();
 
   beforeAll((): void => {
@@ -45,5 +61,50 @@ describe("<Logger /> Test", (): void => {
     renderWithQuery(queryClient);
 
     screen.getByTestId("dashboard-header");
+  });
+
+  it("should have the global-statistics", (): void => {
+    renderWithQuery(queryClient);
+
+    screen.getByText("Confirmed: 10");
+    screen.getByText("Death: 2");
+    screen.getByText("Recovered: 8");
+  });
+
+  it("should filter the countries based on the search-input value", (): void => {
+    renderWithQuery(queryClient);
+
+    fireEvent.change(screen.getByRole("textbox"), {target: {value: "Jordan"}});
+
+    screen.getByText("Jordan");
+    expect(screen.queryByText("Saudi Arabia")).toBeNull();
+  });
+
+  it("should have the county-name column (label&value)", (): void => {
+    renderWithQuery(queryClient);
+
+    screen.getByRole("columnheader", {name: "Country"});
+    screen.getByRole("cell", {name: "Jordan"});
+  });
+
+  it("should have the confirmed-cases column (label&value)", (): void => {
+    renderWithQuery(queryClient);
+
+    screen.getByRole("columnheader", {name: "Confirmed"});
+    screen.getByRole("cell", {name: "100"});
+  });
+
+  it("should have the death-cases column (label&value)", (): void => {
+    renderWithQuery(queryClient);
+
+    screen.getByRole("columnheader", {name: "Death"});
+    screen.getByRole("cell", {name: "20"});
+  });
+
+  it("should have the recovered-cases column (label&value)", (): void => {
+    renderWithQuery(queryClient);
+
+    screen.getByRole("columnheader", {name: "Recovered"});
+    screen.getByRole("cell", {name: "80"});
   });
 });
