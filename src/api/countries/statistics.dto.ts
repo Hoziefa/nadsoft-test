13978b7433d7803e01f78227fb8a937e3bab3baa
Hoziefa Alhassan <hoziefaalhasan@gmail.com>
2023-03-10 11:48:29 +0300

    Handle API missing

--- src/api/countries/statistics.dto.ts
+++ src/api/countries/statistics.dto.ts
@@ -1,13 +1,11 @@
 import {IApiForm, ICountry, IStatistics} from "./statistics.api";
 
 export const statisticsDto = (data: {data: IApiForm}): IStatistics | null => {
-  if (!data?.data.Countries || !data) return null;
-
   return {
     totalConfirmed: data.data.Global.TotalConfirmed,
     totalDeaths: data.data.Global.TotalDeaths,
     totalRecovered: data.data.Global.TotalRecovered,
-    countries: data.data.Countries.map((country): ICountry => ({
+    countries: (data.data.Countries ?? []).map((country): ICountry => ({
       name: country.Country,
       totalConfirmed: country.TotalConfirmed,
       totalDeaths: country.TotalDeaths,
